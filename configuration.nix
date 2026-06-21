4db1d60c7941f4d142ac0d0435e0ce90482cf7c4
sebastian <sebastianwyrostek4@gmail.com>
2026-06-21 14:32:59 +0000

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -61,8 +61,8 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
+  services.displayManager.sddm.enable = false;
+  services.desktopManager.plasma6.enable = false;
 
   # Configure keymap in X11
   services.xserver.xkb = {
